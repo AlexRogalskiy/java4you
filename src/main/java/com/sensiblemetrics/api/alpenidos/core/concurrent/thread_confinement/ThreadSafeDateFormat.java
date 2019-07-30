68886db5ea09f0a145aa4468e4483a673c862e72
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-07-30 06:04:29 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/concurrent/thread_confinement/ThreadSafeDateFormat.java
@@ -0,0 +1,23 @@
+package com.sensiblemetrics.api.alpenidos.core.concurrent.thread_confinement;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Pattern: Thread Local Confinement
+ * <p>
+ * Example: Using a thread safe SimpleDateFormat object
+ */
+public class ThreadSafeDateFormat {
+
+    private static final ThreadLocal<SimpleDateFormat> threadLocalDateFormat = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("DD/MM/YYYY HH:mm:ss");
+        }
+    };
+
+    public String format(Date date) {
+        return threadLocalDateFormat.get().format(date);
+    }
+}
