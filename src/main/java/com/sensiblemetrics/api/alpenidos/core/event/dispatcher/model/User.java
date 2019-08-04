34ce84f4136e5d533f9692efe9de99bfd54c93fb
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-04 03:38:18 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/event/dispatcher/model/User.java
@@ -0,0 +1,18 @@
+package com.sensiblemetrics.api.alpenidos.core.event.dispatcher.model;
+
+import com.sensiblemetrics.api.alpenidos.core.event.dispatcher.event.UserCreatedEvent;
+import com.sensiblemetrics.api.alpenidos.core.event.dispatcher.event.UserUpdatedEvent;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * This {@link User} class is a basic pojo used to demonstrate user data sent along with
+ * the {@link UserCreatedEvent} and {@link UserUpdatedEvent} events.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String username;
+}
