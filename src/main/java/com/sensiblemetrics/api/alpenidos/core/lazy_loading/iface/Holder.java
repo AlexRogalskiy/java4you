58c0679905583e484bf62389ae805981790d70ec
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-04 15:52:17 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/lazy_loading/iface/Holder.java
@@ -0,0 +1,11 @@
+package com.sensiblemetrics.api.alpenidos.core.lazy_loading.iface;
+
+/**
+ * Holder interface declaration
+ *
+ * @param <T> type of holging item
+ */
+public interface Holder<T> {
+
+    T get();
+}
---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/lazy_loading/impl/Java8Holder.java
@@ -0,0 +1,41 @@
+package com.sensiblemetrics.api.alpenidos.core.lazy_loading.impl;
+
+import com.sensiblemetrics.api.alpenidos.core.lazy_loading.iface.Holder;
+import com.sensiblemetrics.api.alpenidos.core.lazy_loading.model.Heavy;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.function.Supplier;
+
+/**
+ * This lazy loader is thread safe and more efficient than {@link HolderThreadSafe}. It utilizes
+ * Java 8 functional interface {@link Supplier} as {@link Heavy} factory.
+ */
+@Slf4j
+public class Java8Holder implements Holder<Heavy> {
+
+    private Supplier<Heavy> heavy = this::createAndCacheHeavy;
+
+    public Java8Holder() {
+        log.info("Java8Holder created");
+    }
+
+    @Override
+    public Heavy get() {
+        return this.heavy.get();
+    }
+
+    private synchronized Heavy createAndCacheHeavy() {
+        class HeavyFactory implements Supplier<Heavy> {
+            private final Heavy heavyInstance = new Heavy();
+
+            @Override
+            public Heavy get() {
+                return this.heavyInstance;
+            }
+        }
+        if (!HeavyFactory.class.isInstance(this.heavy)) {
+            this.heavy = new HeavyFactory();
+        }
+        return this.heavy.get();
+    }
+}
