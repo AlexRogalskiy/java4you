6856ddb3f8627559b9512e33f37b9abe194b523f
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-04 21:07:46 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/map_reduce/master/Master.java
@@ -0,0 +1,81 @@
+package com.sensiblemetrics.api.alpenidos.core.map_reduce.master;
+
+import com.sensiblemetrics.api.alpenidos.core.map_reduce.model.Input;
+import com.sensiblemetrics.api.alpenidos.core.map_reduce.model.Result;
+import com.sensiblemetrics.api.alpenidos.core.map_reduce.worker.Worker;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+
+/**
+ * The abstract Master class which contains private fields numOfWorkers
+ * (number of workers), workers (arraylist of workers), expectedNumResults
+ * (number of divisions of input data, same as expected number of results),
+ * allResultData (hashtable of results obtained from workers, mapped by
+ * their ids) and finalResult (aggregated from allResultData).
+ */
+public abstract class Master {
+    private final int numOfWorkers;
+    private final ArrayList<Worker> workers;
+    private int expectedNumResults;
+    private Hashtable<Integer, Result> allResultData;
+    private Result finalResult;
+
+    public Master(final int numOfWorkers) {
+        this.numOfWorkers = numOfWorkers;
+        this.workers = setWorkers(numOfWorkers);
+        this.expectedNumResults = 0;
+        this.allResultData = new Hashtable<Integer, Result>(numOfWorkers);
+        this.finalResult = null;
+    }
+
+    public Result getFinalResult() {
+        return this.finalResult;
+    }
+
+    Hashtable<Integer, Result> getAllResultData() {
+        return this.allResultData;
+    }
+
+    int getExpectedNumResults() {
+        return this.expectedNumResults;
+    }
+
+    ArrayList<Worker> getWorkers() {
+        return this.workers;
+    }
+
+    public void doWork(final Input input) {
+        this.divideWork(input);
+    }
+
+    private void divideWork(final Input input) {
+        final List<Input> dividedInput = input.divideData(numOfWorkers);
+        if (dividedInput != null) {
+            this.expectedNumResults = dividedInput.size();
+            for (int i = 0; i < this.expectedNumResults; i++) {
+                //ith division given to ith worker in this.workers
+                this.workers.get(i).setReceivedData(this, dividedInput.get(i));
+                this.workers.get(i).run();
+            }
+        }
+    }
+
+    public void receiveData(final Result data, final Worker w) {
+        //check if can receive..if yes:
+        this.collectResult(data, w.getWorkerId());
+    }
+
+    private void collectResult(final Result data, final int workerId) {
+        this.allResultData.put(workerId, data);
+        if (this.allResultData.size() == this.expectedNumResults) {
+            //all data received
+            this.finalResult = this.aggregateData();
+        }
+    }
+
+    protected abstract ArrayList<Worker> setWorkers(int num);
+
+    protected abstract Result aggregateData();
+}
---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/map_reduce/model/ArrayInput.java
@@ -0,0 +1,61 @@
+package com.sensiblemetrics.api.alpenidos.core.map_reduce.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Class ArrayInput extends abstract class {@link Input} and contains data
+ * of type int[][].
+ */
+public class ArrayInput extends Input<int[][]> {
+
+    public ArrayInput(final int[][] data) {
+        super(data);
+    }
+
+    public static int[] makeDivisions(final int[][] data, final int num) {
+        final int initialDivision = data.length / num; //equally dividing
+        final int[] divisions = new int[num];
+        Arrays.fill(divisions, initialDivision);
+        if (initialDivision * num != data.length) {
+            int extra = data.length - initialDivision * num;
+            int l = 0;
+            //equally dividing extra among all parts
+            while (extra > 0) {
+                divisions[l] = divisions[l] + 1;
+                extra--;
+                if (l == num - 1) {
+                    l = 0;
+                } else {
+                    l++;
+                }
+            }
+        }
+        return divisions;
+    }
+
+    @Override
+    public ArrayList<Input> divideData(final int num) {
+        if (this.data == null) {
+            return null;
+        }
+        final int[] divisions = makeDivisions(this.data, num);
+        final ArrayList<Input> result = new ArrayList<Input>(num);
+        int rowsDone = 0; //number of rows divided so far
+        for (int i = 0; i < num; i++) {
+            int rows = divisions[i];
+            if (rows != 0) {
+                int[][] divided = new int[rows][this.data[0].length];
+                for (int j = 0; j < rows; j++) {
+                    divided[j] = this.data[rowsDone + j];
+                }
+                rowsDone += rows;
+                ArrayInput dividedInput = new ArrayInput(divided);
+                result.add(dividedInput);
+            } else {
+                break; //rest of divisions will also be 0
+            }
+        }
+        return result;
+    }
+}
