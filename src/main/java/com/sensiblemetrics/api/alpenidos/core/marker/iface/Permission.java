fed97b7fb54bc2250d839a11a00ee093314734ea
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-04 15:57:36 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/marker/iface/Permission.java
@@ -0,0 +1,8 @@
+package com.sensiblemetrics.api.alpenidos.core.marker.iface;
+
+/**
+ * Interface without any methods
+ * Marker interface is based on that assumption
+ */
+public interface Permission {
+}
