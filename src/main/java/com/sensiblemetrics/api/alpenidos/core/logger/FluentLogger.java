d847a48c89af6042a9ce0319e3fd365d16cd7500
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-09 12:39:54 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/logger/FluentLogger.java
@@ -0,0 +1,86 @@
+package com.sensiblemetrics.api.alpenidos.core.logger;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+public interface FluentLogger {
+    enum Level {
+        DEBUG, WARNING, ERROR
+    }
+
+    default MessageFluent atDebug() {
+        return at(Level.DEBUG);
+    }
+
+    default MessageFluent atWarning() {
+        return at(Level.WARNING);
+    }
+
+    default MessageFluent atError() {
+        return at(Level.ERROR);
+    }
+
+    MessageFluent at(Level level);
+
+    static FluentLogger create(Class<?> declaringClass, Level level) {
+        Objects.requireNonNull(declaringClass);
+        Objects.requireNonNull(level);
+        return new Impl(declaringClass.getName(), level, null);
+    }
+
+    interface MessageFluent {
+        LogFluent message(final String message);
+    }
+
+    interface LogFluent {
+        default void log() {
+            log(System.err::println);
+        }
+
+        void log(final Consumer<? super String> consumer);
+    }
+
+    final /*inline*/ class Impl implements FluentLogger, MessageFluent, LogFluent {
+        private final String className;
+        private final Level level;
+        private final String message;
+
+        public Impl(String className, Level level, String message) {
+            this.className = className;
+            this.level = level;
+            this.message = message;
+        }
+
+        @Override
+        public MessageFluent at(Level level) {
+            Objects.requireNonNull(level);
+            return new Impl(className, isEnabled(this.level, level) ? level : null, null);
+        }
+
+        private static boolean isEnabled(Level loggerLevel, Level loggingLevel) {
+            if (loggerLevel == Level.DEBUG) {
+                return true;
+            }
+            if (loggerLevel == Level.WARNING) {
+                return loggingLevel == Level.WARNING || loggingLevel == Level.ERROR;
+            }
+            //if (loggerLevel == Level.ERROR) {
+            return loggingLevel == Level.ERROR;
+            //}
+        }
+
+        @Override
+        public LogFluent message(String message) {
+            Objects.requireNonNull(message);
+            return new Impl(className, level, message);
+        }
+
+        @Override
+        public void log(Consumer<? super String> consumer) {
+            Objects.requireNonNull(consumer);
+            if (level != null) {
+                consumer.accept(level + " " + message + " at " + className);
+            }
+        }
+    }
+}
