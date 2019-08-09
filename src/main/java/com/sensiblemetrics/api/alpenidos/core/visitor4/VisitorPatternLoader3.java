a4db9e2c0d2c83435f4b7b378be8fc59c9b10276
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-09 03:37:17 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/visitor4/VisitorPatternLoader3.java
@@ -0,0 +1,44 @@
+package com.sensiblemetrics.api.alpenidos.core.visitor4;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+public class VisitorPatternLoader3 {
+
+    public interface Vehicle {
+    }
+
+    public static class Car implements Vehicle {
+    }
+
+    public static class Moto implements Vehicle {
+    }
+
+    public static class Visitor<R> {
+        private final Map<Class<?>, Function<Object, ? extends R>> map = new HashMap<>();
+
+        public <T> Visitor<R> when(final Class<? extends T> type, final Function<? super T, ? extends R> fun) {
+            this.map.put(type, (Function<Object, ? extends R>) fun);
+            return this;
+        }
+
+        public R call(final Object receiver) {
+            return this.map.getOrDefault(receiver.getClass(),
+                obj -> {
+                    throw new IllegalArgumentException("invalid " + obj);
+                })
+                .apply(receiver);
+        }
+    }
+
+    public static void main(final String[] args) {
+        final Visitor<String> visitor = new Visitor<>();
+        visitor.when(Car.class, car -> "car")
+            .when(Moto.class, moto -> "moto");
+
+        final Vehicle vehicle = new Car();
+        final String text = visitor.call(vehicle);
+        System.out.println(text);
+    }
+}
