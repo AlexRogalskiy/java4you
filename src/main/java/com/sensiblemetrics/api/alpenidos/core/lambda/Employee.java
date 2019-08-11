a7d427f6d02312347539e01fb8a9683889e9488c
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-11 03:51:29 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/lambda/Employee.java
@@ -0,0 +1,10 @@
+package com.sensiblemetrics.api.alpenidos.core.lambda;
+
+import lombok.Data;
+
+@Data
+public class Employee {
+    private String name;
+    private String id;
+    private String Designation;
+}
