8efd939ff76e2716fbab86c760d0c465c5ac8b77
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2019-08-18 02:48:37 +0300

    Changeset to repo

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/path_finder/repository/GraphDAO.java
@@ -0,0 +1,24 @@
+package com.sensiblemetrics.api.alpenidos.core.path_finder.repository;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.RandomUtils;
+
+import java.util.List;
+
+public class GraphDAO {
+
+    public List<String> listLocations() {
+        return Lists.newArrayList(
+            "CNHKG", "AUMEL", "SESTO", "FIHEL", "USCHI", "JNTKO", "DEHAM", "CNSHA", "NLRTM", "SEGOT", "CNHGH", "USNYC", "USDAL"
+        );
+    }
+
+    public String getVoyageNumber(final String from, final String to) {
+        final int i = RandomUtils.nextInt(0, 5);
+        if (i == 0) return "0100S";
+        if (i == 1) return "0200T";
+        if (i == 2) return "0300A";
+        if (i == 3) return "0301S";
+        return "0400S";
+    }
+}
