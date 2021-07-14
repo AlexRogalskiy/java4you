ed33f0e9b99bbfdd46bf9aba03b4175d5e9aede3
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-07-14 14:02:18 +0300

    Updates on pom lib versions
    Added updates on camel spring boot version

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/proxy7/EmployeeInterface.java
@@ -0,0 +1,16 @@
+package com.sensiblemetrics.api.alpenidos.core.proxy7;
+
+public interface EmployeeInterface {
+
+    String getName();
+
+    void setName(String name);
+
+    int getRating();
+
+    void setRating(int rating);
+
+    int getSalary();
+
+    void setSalary(int salary);
+}
---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/proxy7/EmployeeInvocationHandler.java
@@ -0,0 +1,50 @@
+package com.sensiblemetrics.api.alpenidos.core.proxy7;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class EmployeeInvocationHandler implements InvocationHandler {
+
+    private final EmployeeInterface emp;
+
+    public EmployeeInvocationHandler(EmployeeInterface emp) {
+        this.emp = emp;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getName().contains("setSalary")) {
+            throw new IllegalAccessException();
+        }
+        if (method.getName().startsWith("get")) {
+            return method.invoke(emp, args);
+        }
+
+        return null;
+    }
+}
+
+
+class ManagerInvocationHandler implements InvocationHandler {
+
+    private final EmployeeInterface emp;
+
+    public ManagerInvocationHandler(EmployeeInterface emp) {
+        this.emp = emp;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        if (method.getName().contains("Salary")) {
+            throw new IllegalAccessException();
+        }
+        if (method.getName().startsWith("get")) {
+            return method.invoke(emp, args);
+        }
+        if (method.getName().startsWith("set")) {
+            return method.invoke(emp, args);
+        }
+
+        return null;
+    }
+}
