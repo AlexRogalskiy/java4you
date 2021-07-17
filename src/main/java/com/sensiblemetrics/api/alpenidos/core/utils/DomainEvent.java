0e3a179cb79217bbdb72a74d50fe8b01ea55670c
AlexRogalskiy <a.rogalskii@bftcom.com>
2021-07-18 01:12:49 +0300

    Updates on pom lib versions
    Added updates on camel spring boot version

---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/utils/DomainEvent.java
@@ -0,0 +1,13 @@
+package com.sensiblemetrics.api.alpenidos.core.utils;
+
+import java.time.Instant;
+import java.util.UUID;
+
+public interface DomainEvent {
+
+    UUID getEventId();
+
+    UUID getAggregateId();
+
+    Instant getWhen();
+}
---
+++ src/main/java/com/sensiblemetrics/api/alpenidos/core/utils/EitherResult.java
@@ -0,0 +1,17 @@
+package com.sensiblemetrics.api.alpenidos.core.utils;
+
+import io.vavr.control.Either;
+
+import static io.vavr.control.Either.left;
+import static io.vavr.control.Either.right;
+
+public class EitherResult {
+
+    public static <L, R> Either<L, R> announceFailure(L left) {
+        return left(left);
+    }
+
+    public static <L, R> Either<L, R> announceSuccess(R right) {
+        return right(right);
+    }
+}
